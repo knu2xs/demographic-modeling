8b59b96d45b59d3c41049d9109e497458f5a4229
Joel McCune <knu2xs@gmail.com>
2021-04-05 09:11:52 -0700

    fix __init__.py for bumpversion

--- src/modeling/__init__.py
+++ src/modeling/__init__.py
@@ -1,3 +1,8 @@
+__title__ = 'demographic-modeling-module'
+__version__ = '0.1.0-dev0'
+__author__ = 'Joel McCune'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2020 by Joel McCune'
 """
 The modeling module provides capabilities supporting quantitative geographic analysis.
 """
