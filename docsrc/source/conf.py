4119201b62c461e924998143d93dc5c2f59f799a
Joel McCune <knu2xs@gmail.com>
2021-04-08 15:37:04 -0700

    Bump version: 0.2.0-dev0 → 0.3.0-dev0

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -30,7 +30,7 @@ copyright = '2020, Joel McCune'
 author = 'Joel McCune'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0-dev0'
+release = '0.3.0-dev0'
 
 
 # -- General configuration -----------------------------------------------------
